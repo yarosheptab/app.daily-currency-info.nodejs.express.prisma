bd02f0507182f814c8d8251c87d7f58d3c97cb25
bohdan.yaroshepta <yaroshepta.b@gmail.com>
2024-05-19 00:01:43 +0300

    Added /subscribe route with all related logic

--- routes/currency.js
+++ routes/currency.js
@@ -2,13 +2,45 @@ const express = require("express");
 const { HttpStatusCode } = require("axios");
 const { getTodayCurrencyRate } = require("../controllers/currency");
 const { UNEXPECTED_ERROR_MESSAGE } = require("../utils/constants");
+const { createSubscriber } = require("../prisma/queries");
+const { z } = require("zod");
 const router = express.Router();
+
+const FormSchema = z.object({
+    email: z
+        .string({ message: "Email should be a string." })
+        .email({ message: "Provide a valid email." }),
+});
 
 router.get("/today", async (req, res) => {
     try {
         const todayCurrencyRate = await getTodayCurrencyRate();
 
         res.status(HttpStatusCode.Ok).send(todayCurrencyRate);
+    } catch (error) {
+        console.log(error);
+        res.status(HttpStatusCode.InternalServerError).send({
+            message: error?.message ?? UNEXPECTED_ERROR_MESSAGE,
+        });
+    }
+});
+
+router.post("/subscribe", async (req, res) => {
+    try {
+        const requestBody = req.body;
+
+        const validation = FormSchema.safeParse(requestBody);
+
+        if (!validation.success) {
+            return res.status(HttpStatusCode.UnprocessableEntity).send({
+                errors: validation.error.errors,
+            });
+        }
+
+        await createSubscriber(requestBody.email);
+        res.status(HttpStatusCode.Ok).send({
+            message: "Thank you for subscribing!",
+        });
     } catch (error) {
         console.log(error);
         res.status(HttpStatusCode.InternalServerError).send({
